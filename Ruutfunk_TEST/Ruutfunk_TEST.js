ef9176399de3ecc231cdb60ba7f2fe2426b1372c
ArturFromMaurus <97187315+ArturFromMaurus@users.noreply.github.com>
2023-03-17 03:41:02 +0200

    window.onload

--- Ruutfunk_TEST/Ruutfunk_TEST.js
+++ Ruutfunk_TEST/Ruutfunk_TEST.js
@@ -20,7 +20,9 @@ var table;
 
 // ----------------------------------------- HTML ToolTip -------------------------------------------
 
-var tooltip = document.createElement("div");
+window.onload = function (){
+
+tooltip = document.createElement("div");
 tooltip.style.backgroundColor = "rgba(9,9,96,0.85)"
 tooltip.style.color = "white";
 tooltip.style.padding = "10px";
@@ -32,19 +34,19 @@ tooltip.style.width="600px";
 tooltip.style.borderRadius="25px"
 document.body.appendChild(tooltip);
 
-var regularText = document.createElement("div");
+regularText = document.createElement("div");
 regularText.innerHTML = "Punkti saab joonisele märkida hiireklõpsuga.<br>Punkt A vastab tabeli esimesele veerule ning punkt B teisele jne.<br>Punktide eemaldamiseks tuleb hiirt veel kord klõpsata (kokku 8 klõpsu) ja alustada uuesti.<br>Tabeli neljandasse veergu paiguta haripunkt. Tabeli teisse ja eelviimasesse veergu nullkohad.<br><br>Testi eest saab +1 punkti kui tabel ning joonis on edukalt loodud.";
 regularText.style.fontFamily="Computer Modern";
 regularText.style.fontSize="20px";
 tooltip.appendChild(regularText);
 
 KaTeX_EQ=''
-var katexEquation = document.createElement("div");
+katexEquation = document.createElement("div");
 tooltip.appendChild(katexEquation);
 
 
 // Info nuppu funktsionaalsus
-var infoNupp = document.createElement("button");
+infoNupp = document.createElement("button");
 infoNupp.innerHTML = "i";
 infoNupp.style.position = "absolute";
 infoNupp.style.margin="20px";
@@ -72,7 +74,8 @@ infoNupp.addEventListener("mouseleave", function() {
   tooltip.style.display = "none";
   infoNupp.style.background="transparent"
 });
-
+  
+}
 // ----------------------------------------- HTML ToolTip -------------------------------------------
 
 
